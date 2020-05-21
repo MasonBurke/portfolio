f3687d796502954a6318b1d178569f1440e77427
Mason Burke <masonburke@rocketmail.com>
2020-05-21 17:53:28 -0600

    navbar created

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+import './App.css';
+import Navbar from "../src/components/Navbar"
+
+function App() {
+  return (
+<Router>
+
+  <div>
+<Navbar />
+  </div>
+
+</Router>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lt navbar-dark bg-dark">
+      <Link activeClassName="`is-active" to="`/`" className="navbar-brand" to="/">
+        Wikipedia Searcher
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/home"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link activeClassName="`is-active"
+              to="/projects"
+              className={window.location.pathname === "/signup" ? "nav-link active" : "nav-link"}
+            >
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
