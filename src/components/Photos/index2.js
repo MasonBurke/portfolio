8ba4c70797f0ba633f07ee7b54febd89c8c3dcd0
Mason Burke <masonburke@rocketmail.com>
2020-05-30 14:42:32 -0600

    done for now. WOP

--- src/components/Photos/index2.js
+++ src/components/Photos/index2.js
@@ -3,53 +3,60 @@ import Proj1 from "./Proj1.png"
 import Weather from "./Weather.png"
 import Todo from "./Todo.png"
 
-const styles =  {
-img:{
-    height: 255,
-    width: 255,
-    
-},
-h5:{
-   marginLeft: 48
-},
-h6:{
-    marginLeft: 48
+const styles = {
+    img: {
+        height: 199,
+        width: 199,
 
-},
-card:{
-    margin:15,
+    },
+    h5: {
+        marginLeft: `28%`
+    },
+    h6: {
+        marginLeft: `33%`
 
+    },
+    card: {
+        margin: 15,
 
-}
+    },
+    wrapper: {
+        display: `flex`,
+        justifyContent: `center`,
+
+    }
 };
 
 
 function Proj() {
-    return(
-<>
-<div className="card" style={styles.card}>
-<img style={styles.img} src={Proj1}/>
-<h5 style={styles.h5}>The Ultimate IQ Test</h5>
-<h6 style={styles.h6}>Code</h6>
-{/* <Route exact path="/" render={() => (window.location = "https://github.com/MasonBurke/proj2")}/> */}
-</div>
+    return (
+        <>
+            <div style={styles.wrapper}>
 
-<div className="card" style={styles.card}>
-<img style={styles.img} src={Weather}/>
-<h5 style={styles.h5}>Weather App</h5>
-<h6 style={styles.h6}>Code</h6>
-{/* <Route exact path="/" render={() => (window.location = "https://github.com/MasonBurke/proj2")}/> */}
-</div>
+                <div className="card" style={styles.card}>
+                    <img style={styles.img} src={Proj1} />
+                    <h5 style={styles.h5}>The Ultimate IQ Test</h5>
+                    <h6 style={styles.h6}>Code</h6>
+                    {/* <Route exact path="/" render={() => (window.location = "https://github.com/MasonBurke/proj2")}/> */}
+                </div>
 
-<div className="card" style={styles.card}>
-<img style={styles.img} src={Todo}/>
-<h5 style={styles.h5}>Git 'er Done! (To do list) </h5>
-<h6 style={styles.h6}>Code</h6>
-{/* <Route exact path="/" render={() => (window.location = "https://github.com/MasonBurke/proj2")}/> */}
-</div>
+                <div className="card" style={styles.card}>
+                    <img style={styles.img} src={Weather} />
+                    <h5 style={styles.h5}>Weather App</h5>
+                    <h6 style={styles.h6}>Code</h6>
+                    {/* <Route exact path="/" render={() => (window.location = "https://github.com/MasonBurke/proj2")}/> */}
+                </div>
+
+                <div className="card" style={styles.card}>
+                    <img style={styles.img} src={Todo} />
+                    <h5 style={styles.h5}>Git 'er Done! (To do list) </h5>
+                    <h6 style={styles.h6}>Code</h6>
+                    {/* <Route exact path="/" render={() => (window.location = "https://github.com/MasonBurke/proj2")}/> */}
+                </div>
+            </div>
 
 
-</>
+        </>
 
     )
 }
--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,10 +1,43 @@
 import React from "react"
+import { Redirect } from "react-router-dom"
+
+const styles = {
+    all: {
+        display: `flex`,
+        justifyContent: `center`,
+        height: 55
+    }
+}
+
+
+
 
 function Contact() {
     return (
-        <div>
-
+        <> <div style={styles.all}>
+            <svg class="bi bi-code" width="2.5em" height="2.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.854 4.146a.5.5 0 0 1 0 .708L2.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm4.292 0a.5.5 0 0 0 0 .708L13.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z" />
+            </svg>
         </div>
+            <h6 style={styles.all}>www.github.com/MasonBurke</h6>
+
+            <div style={styles.all}>
+                <svg class="bi bi-phone" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M11 1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                    <path fill-rule="evenodd" d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                </svg>
+            </div>
+            <h6 style={styles.all}>314-607-3102</h6>
+
+            <div style={styles.all}>
+
+                <svg class="bi bi-envelope" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M14 3H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z" />
+                    <path d="M.05 3.555C.017 3.698 0 3.847 0 4v.697l5.803 3.546L0 11.801V12c0 .306.069.596.192.856l6.57-4.027L8 9.586l1.239-.757 6.57 4.027c.122-.26.191-.55.191-.856v-.2l-5.803-3.557L16 4.697V4c0-.153-.017-.302-.05-.445L8 8.414.05 3.555z" />
+                </svg>
+            </div>
+            <h6 style={styles.all}>MasonBurke@RocketMail.com</h6>
+        </>
     )
 
 }
