30ee049970123d8d12dacf1442d2c413a01b55dc
Mason Burke <masonburke@rocketmail.com>
2020-06-01 08:49:48 -0600

    Updates

---
+++ precache-manifest.9a88b3968e2d3615a055862d9351c4a7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70322e4ee5dc8c31140739dfadf82edb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ec0a8ca5622c68c823b0",
+    "url": "/portfolio/static/css/main.f46805ff.chunk.css"
+  },
+  {
+    "revision": "68b2ba40253b2f9440ba",
+    "url": "/portfolio/static/js/2.b9869bea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.b9869bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0a8ca5622c68c823b0",
+    "url": "/portfolio/static/js/main.925269aa.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "9261039ed1c050c48186472637aaf5c5",
+    "url": "/portfolio/static/media/Headshot.9261039e.jpg"
+  },
+  {
+    "revision": "ec448091f7791160c35e34f7b721d274",
+    "url": "/portfolio/static/media/Proj1.ec448091.png"
+  },
+  {
+    "revision": "637418c45bf5a0b13f3acdcc3ae342fe",
+    "url": "/portfolio/static/media/Todo.637418c4.png"
+  },
+  {
+    "revision": "1da674c647f159a321f2f2331c951382",
+    "url": "/portfolio/static/media/Weather.1da674c6.png"
+  }
+]);
